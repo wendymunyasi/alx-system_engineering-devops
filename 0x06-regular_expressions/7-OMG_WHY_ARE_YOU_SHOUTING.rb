49e9e0fa499aaebe56e917d87d360c6e86c60451
wendy munyasi <wendymunyasi@gmail.com>
2022-07-26 15:37:26 +0300

    Add script with regex - task 7

---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# This script accepts one argument and passes it to a regular expression,
+# matching method
+# The regular expression must be only matching: capital letters
+
+puts ARGV[0].scan(/[A-Z]/).join
