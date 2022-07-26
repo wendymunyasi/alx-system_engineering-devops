890e042c8b8c9a8971507bf7bf770fdc90f169e3
wendy munyasi <wendymunyasi@gmail.com>
2022-07-26 15:48:45 +0300

    Fix script with regex - task 8 - try 2

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -3,4 +3,4 @@
 # matching method
 # This script should output: [SENDER],[RECEIVER],[FLAGS]
 
-puts ARGV[0].scan(/\[from:(\+?(?:\w)+)\]\s\[to:(\+?(?:\w)+)\]\s\[flags:((?:-?\d:?)+)\]/).join(separator=",")"
+puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')
