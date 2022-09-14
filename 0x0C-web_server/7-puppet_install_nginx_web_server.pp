f945d7891346e61b04972f040d19a4b59c5de539
wendy munyasi <wendymunyasi@gmail.com>
2022-09-15 01:13:05 +0300

    Add I am so tired 2

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,12 @@
+# Installs and configure an Nginx server using Puppet instead of Bash
+# Nginx should be listening on port 80
+# When querying Nginx at its root / with a GET request (requesting a page)
+# using curl, it must return a page that contains the string Hello World!
+# The redirection must be a “301 Moved Permanently”
+# Your answer file should be a Puppet manifest containing commands to
+# automatically configure an Ubuntu machine to respect above requirements
+
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/luischaparroc permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+}
