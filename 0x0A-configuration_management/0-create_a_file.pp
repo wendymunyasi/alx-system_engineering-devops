5efeec13cb2aa19112f48ed197fe1fd328ab119f
wendy munyasi <wendymunyasi@gmail.com>
2022-09-16 13:31:50 +0300

    Create a file in /tmp - task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# manifest to create file in tmp directory
+file { '/tmp/school':
+  ensure  => present,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
