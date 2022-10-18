c28d7a3e0d4b1b28ca0cc6a1a8e954d198362837
wendy munyasi <wendymunyasi@gmail.com>
2022-10-18 16:57:06 +0300

    Add script that exports data in csv format - task1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""script that fetches info about a given employee's ID using an api"""
+import json
+import requests
+import sys
+
+
+base_url = 'https://jsonplaceholder.typicode.com'
+
+if __name__ == "__main__":
+
+    user_id = sys.argv[1]
+
+    # get user info e.g https://jsonplaceholder.typicode.com/users/1/
+    user_url = '{}/users?id={}'.format(base_url, user_id)
+    # print("user url is: {}".format(user_url))
+
+    # get info from api
+    response = requests.get(user_url)
+    # pull data from api
+    data = response.text
+    # parse the data into JSON format
+    data = json.loads(data)
+    # extract user data, in this case, username of employee
+    user_name = data[0].get('username')
+    # print("id is: {}".format(user_id))
+    # print("name is: {}".format(user_name))
+
+    # get user info about todo tasks
+    # e.g https://jsonplaceholder.typicode.com/users/1/todos
+    tasks_url = '{}/todos?userId={}'.format(base_url, user_id)
+    # print("tasks url is: {}".format(tasks_url))
+
+    # get info from api
+    response = requests.get(tasks_url)
+    # pull data from api
+    tasks = response.text
+    # parse the data into JSON format
+    tasks = json.loads(tasks)
+
+    # build the csv
+    builder = ""
+    for task in tasks:
+        builder += '"{}","{}","{}","{}"\n'.format(
+            user_id,
+            user_name,
+            task['completed'],  # or use get method
+            task['title']
+        )
+    with open('{}.csv'.format(user_id), 'w', encoding='UTF8') as myFile:
+        myFile.write(builder)
