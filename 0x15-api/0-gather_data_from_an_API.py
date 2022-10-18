679b2a7b8a1e6ab0998067c36a33e09cdb9ad43d
wendy munyasi <wendymunyasi@gmail.com>
2022-10-18 15:15:49 +0300

    Add script that fetches data from api - task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""script that fetches info about a given employee's ID using an api"""
+import json
+import sys
+
+import requests
+
+base_url = 'https://jsonplaceholder.typicode.com'
+
+if __name__ == "__main__":
+
+    user_id = sys.argv[1]
+
+    # get user info e.g https://jsonplaceholder.typicode.com/users/1/
+    user_url = '{}/users?id={}'.format(base_url, user_id)
+    # print("user url is: {}".format(user_url))
+
+    # get info from api
+    response = requests.get(user_url)
+    # pull data from api
+    data = response.text
+    # parse the data into JSON format
+    data = json.loads(data)
+    # extract user data, in this case, name of employee
+    user_name = data[0].get('name')
+    # print("id is: {}".format(user_id))
+    # print("name is: {}".format(user_name))
+
+    # get user info about todo tasks
+    # e.g https://jsonplaceholder.typicode.com/users/1/todos
+    tasks_url = '{}/todos?userId={}'.format(base_url, user_id)
+    # print("tasks url is: {}".format(tasks_url))
+
+    # get info from api
+    response = requests.get(tasks_url)
+    # pull data from api
+    tasks = response.text
+    # parse the data into JSON format
+    tasks = json.loads(tasks)
+
+    # initialize completed count as 0 and find total number of tasks
+    completed = 0
+    total_tasks = len(tasks)
+
+    # initialize empty list for completed tasks
+    completed_tasks = []
+    # loop through tasks counting number of completed tasks
+    for task in tasks:
+
+        if task.get('completed'):
+            # print("The tasks are: {}\n".format(task))
+            completed_tasks.append(task)
+            completed += 1
+
+    # print the output in the required format
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user_name, completed, total_tasks))
+    for task in completed_tasks:
+        print("\t {}".format(task.get('title')))
