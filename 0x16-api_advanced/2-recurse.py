a750e5288b34a0aa01ce7724a1bec3b3b0403166
wendy munyasi <wendymunyasi@gmail.com>
2022-12-09 13:22:58 +0300

    Make some changes

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -18,35 +18,15 @@ def recurse(subreddit, hot_list=[], n=0, after=None):
     Returns:
         list: list of titles.
     """
-    base_url = 'https://www.reddit.com'
-    url = '{}/r/{}/.json?sort={}&limit={}&count={}&after={}'.format(
-        base_url,
-        subreddit,
-        sort,
-        limit,
-        n,
-        after if after else ''
-    )
-    headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
-    }
-    sort = 'hot'
-    limit = 30
-    res = requests.get(
-        url,
-        headers=headers,
-        allow_redirects=False
-    )
-    if res.status_code == 200:
-        data = res.json()['data']
-        posts = data['children']
-        count = len(posts)
-        hot_list.extend(list(map(lambda x: x['data']['title'], posts)))
-        if count >= limit and data['after']:
-            return recurse(subreddit, hot_list, n + count, data['after'])
-        else:
-            return hot_list if hot_list else None
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'user-agent': 'custom'}
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 200:
+        r = r.json()
+        for post in r.get('data').get('children'):
+            hot_list.append(post.get('data').get('title'))
+        if r.get('data').get('after'):
+            recurse(subreddit, hot_list)
+        return hot_list
     else:
-        return hot_list if hot_list else None
+        return None
